b8b34c1f0e57795c9b27e2cd9889042a201d6590
Jay Bhesania <jaybhesania@gmail.com>
2025-05-24 22:33:58 -0400

    upload files

---
+++ train_dump_model.py
@@ -0,0 +1,15 @@
+import joblib
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+
+# Load the iris dataset
+
+iris = load_iris()
+X, y = iris.data, iris.target
+
+# Train a model
+model = RandomForestClassifier()
+model.fit(X, y)
+
+# Save the trained model
+joblib.dump(model, 'model.joblib')
